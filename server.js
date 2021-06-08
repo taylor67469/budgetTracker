e18eb787b5dd7e15c0b0fab9b00df03fe3c1ddf1
Zachary <taylor67469@yahoo.com>
2021-06-08 18:21:02 -0500

    fixing heroku stuff

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const uri= "mongodb+srv://admin:password1234@cluster0.fckkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = 3000||process.env.PORT;
 
 const app = express();
@@ -15,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(uri,"mongodb://localhost/budget",{
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget",{
   useNewUrlParser: true,
   useFindAndModify: false
 });
